c313808622fcd2af05df021d1c795b641b42a75c
dysbulic <dys@dhappy.org>
2025-06-13 23:00:35 -0400

    addin’ global stylesheet 🌺

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Hemp Smoker's Deluxe Kit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<style>
+  h1 {
+    font-family: 'Brilliant Gemstone', sans-serif;
+    text-align: center;
+    font-size: clamp(55pt, 12vw, 88pt);
+    color: transparent;
+    background-clip: text;
+    background-image: linear-gradient(to right, #ff7e5f, #199810);
+    text-fill-color: transparent;
+  }
+</style>
